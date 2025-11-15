a35f1dfc4e18da854a79988e8ece429195bfd649
Arthur Freitas Ramos <arfreita@microsoft.com>
2025-11-14 23:37:22 -0500

    Revert "Document circle encode-decode progress and helpers"
    This reverts commit 0bc44005ce7db0c4d5accda3cc0896dc00ca0476.

--- Scripts/IntAddInspect.lean
+++
@@ -1,8 +0,0 @@
-import Init.Core
-
-#print Int.add
-#print Int.subNatNat
-#print Nat.sub_add_cancel
-#print Nat.sub_eq
-#print Nat.sub_eq_zero_of_le
-#print Nat.sub_ne_zero_of_lt
--- Scripts/IntLemmas.lean
+++
@@ -1,5 +0,0 @@
-import Init.Core
-
-variable (m : Int) (k : Nat)
-
-#reduce m + Int.ofNat k.succ
